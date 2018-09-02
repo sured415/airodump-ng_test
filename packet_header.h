169af74552b2cf07e8a07f76895d70dc496bc35e
sured415 <wtghnh@gmail.com>
2018-09-02 13:54:49 -0400

    add packet_header.h

---
+++ packet_header.h
@@ -0,0 +1,36 @@
+#ifndef PACKET_HEAER_H
+#define PACKET_HEAER_H
+
+#endif // PACKET_HEAER_H
+
+
+struct radiotap_header {
+    uint8_t it_version;
+    uint8_t it_pad;
+    uint16_t it_len;
+    uint32_t it_present1;
+    uint32_t it_present2;
+    uint8_t flags;
+    uint8_t data_rate;
+    uint16_t ch_freq;
+    uint16_t ch_flag;
+    uint8_t antenna_signal1;
+    uint16_t rx_flag;
+    uint8_t antenna_signal2;
+    uint8_t antenna;
+};
+
+struct IEEE80211_beacon {
+    uint16_t fc;                /* Frame Control */
+    uint16_t duration;
+    uint8_t da[6];
+    uint8_t sa[6];
+    uint8_t bssid[6];           /* BSSID */
+    uint16_t seq_control;
+};
+
+struct IEEE80211_framebody_NIE {
+    uint64_t timestamp;
+    uint16_t bi;                /* Beacon Interval */
+    uint16_t ci;                 /* Capability Information */
+};
